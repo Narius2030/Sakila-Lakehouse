f329f6539a6ca04f0084d3de116c7bd8272aed1d
Narius2030 <buiducnhan15122003@gmail.com>
2025-01-22 09:09:50 +0700

    config: add debezium config

---
+++ kafka/conf/initialize.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+initialize() {
+  echo "===> Building connector config from env vars ..."
+  python /setup/build_config.py | python -m json.tool > connector_config.json
+
+  echo "===> Uploading config to connector '${CONNECTOR_NAME}':"
+  sed -e 's/\(.*password.*"\).*\("\)/\1*****\2/g' connector_config.json
+
+  success=1
+  while [ "$success" != "0" ]
+  do
+    sleep 5
+    echo "===> Trying to upload connector config ..."
+    curl -X PUT -fsS -o /dev/null \
+      -H "Accept:application/json" \
+      -H "Content-Type:application/json" \
+      -d @connector_config.json \
+      "http://${CONNECT_REST_ADVERTISED_HOST_NAME}:${CONNECT_REST_PORT}/connectors/${CONNECTOR_NAME}/config"
+
+    success=$?
+  done
+  echo "===> Successfully configured connector '${CONNECTOR_NAME}'"
+}
+
+initialize & 
+
+exec /docker-entrypoint.sh "start"
--- notebook/work/streamify/streaming_functions.py
+++ notebook/work/streamify/streaming_functions.py
@@ -3,13 +3,13 @@ from pyspark.sql.functions import *
 from schema import schema_taxi
 
 def process_stream(stream):
-    stream = stream \
-              .selectExpr("CAST(value AS STRING)") \
-              .select(from_json(col("value"), schema_taxi).alias("data"))
-    
-    stream = stream \
-            .groupBy("data.pickup_location")\
-            .agg(avg("data.trip_distance").alias("avg_trip_distance"), avg("data.total_amount").alias("avg_total_amount"))\
-    
-    return stream
+	stream = stream \
+			.selectExpr("CAST(value AS STRING)") \
+			.select(from_json(col("value"), schema_taxi).alias("data"))
+
+	stream = stream \
+		.groupBy("data.pickup_location")\
+		.agg(avg("data.trip_distance").alias("avg_trip_distance"), avg("data.total_amount").alias("avg_total_amount"))
+
+	return stream
                 
