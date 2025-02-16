f3e975a47ca6579d5b7769b63f0a2087cb94c3af
Narius2030 <buiducnhan15122003@gmail.com>
2025-02-16 16:17:54 +0700

    feat: create source.yml

--- dbt-airflow/models/example/my_first_dbt_model.sql
+++ dbt-airflow/models/example/my_first_dbt_model.sql
@@ -1,29 +1,10 @@
 
 /*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
+    {{ config(
+        unique_key='id',
+        incremental_srategy='merge'
+    ) }}
 */
 
 
-{{ config(
-    unique_key='id',
-    incremental_strategy='append'
-) }}
-
-
-with source_data as (
-    select 1 as id, 'narius' as user_name
-    union all
-    select 2 as id, 'andy' as user_name
-)
-
-select * from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+select * from {{ source('delta-streamify', 'payment') }}
--- dbt-airflow/models/example/my_second_dbt_model.sql
+++ dbt-airflow/models/example/my_second_dbt_model.sql
@@ -1,11 +1,11 @@
+/*
+    {{ config(
+        unique_key='id',
+        incremental_srategy='merge'
+    ) }}
+*/
 
--- Use the `ref` function to select from other models
-
-{{ config(
-    unique_key='id',
-    incremental_strategy='append'
-) }}
 
 select *
 from {{ ref('my_first_dbt_model') }}
-where id = 1
+where id < 100
